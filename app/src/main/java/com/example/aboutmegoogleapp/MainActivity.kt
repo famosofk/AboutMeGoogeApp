825686180fd5d3592189692761e9f782578f7ed6
FamosoFK <Fabianogmjunior@gmail.com>
2020-06-19 12:20:05 -0300

    Including linear layout
    I'm trying this to see if it's gonna be counted. At the moment I've 2 contributions, but in dice roller i've commited 7 times.

--- app/src/main/java/com/example/aboutmegoogleapp/MainActivity.kt
+++ app/src/main/java/com/example/aboutmegoogleapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.aboutmegoogleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayout : LinearLayout = findViewById(R.id.linearLayout)
     }
 }
